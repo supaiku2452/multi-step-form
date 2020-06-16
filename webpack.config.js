d78bbdcb4e8ce23a843bd1ae2d6a8e396f4be6b8
supaiku2452 <supaiku2452@gmail.com>
2020-06-17 02:04:52 +0900

    Change from js to ts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve("dist"),
     filename: "bundle.js",
   },
   resolve: {
     modules: ["node_modules", path.join(__dirname, "src")],
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              experimentalWatchApi: true,
+              transpileOnly: true,
+            },
+          },
+        ],
       },
     ],
   },
